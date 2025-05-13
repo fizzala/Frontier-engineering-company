cfecac89e78f5b836a9c0f76d3fe4cd444873103
Fizza <162970932+fizzala@users.noreply.github.com>
2025-05-13 11:41:43 +0500

    index file added

---
+++ index.php
@@ -0,0 +1,359 @@
+<?php
+ include 'header.php'
+?>
+<!DOCTYPE html>
+<html lang="en">
+<body>
+   <!-- banner section start -->
+   <div class="banner_section">
+      <div class="banner-section-2">
+         <div class="container">
+            <div id="main_slider" class="carousel slide" data-ride="carousel">
+               <div class="carousel-inner">
+                  <div class="carousel-item active">
+                     <div class="container">
+                        <div class="row">
+                           <div class="col-md-12">
+                              <h1 class="banner_taital">Frontier Engineering And Automation Consultant</h1>
+                              <p class="banner_text">Making homes smarter and industries sharper with cutting-edge
+                                 automation and engineering expertise.</p>
+                              <div class="read_bt"><a href="#">Read More</a></div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="carousel-item">
+                     <div class="container">
+                        <div class="row">
+                           <div class="col-md-12">
+                              <h1 class="banner_taital">Frontier Engineering And Automation Consultant</h1>
+                              <p class="banner_text">Making homes smarter and industries sharper with cutting-edge
+                                 automation and engineering expertise.</p>
+                              <div class="read_bt"><a href="#">Read More</a></div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="carousel-item">
+                     <div class="container">
+                        <div class="row">
+                           <div class="col-md-12">
+                              <h1 class="banner_taital">Frontier Engineering And Automation Consultant </h1>
+                              <p class="banner_text">Making homes smarter and industries sharper with cutting-edge
+                                 automation and engineering expertise.</p>
+                              <div class="read_bt"><a href="#">Read More</a></div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+               <a style="color:blue" class="carousel-control-prev" href="#main_slider" role="button" data-slide="prev">
+                  <i class="fa fa-angle-left"></i>
+               </a>
+               <a style="color:blue" class="carousel-control-next" href="#main_slider" role="button" data-slide="next">
+                  <i class="fa fa-angle-right"></i>
+               </a>
+            </div>
+         </div>
+      </div>
+   </div>
+   <!-- banner section end -->
+   <!-- service section start -->
+   <div class="service_section layout_padding">
+      <div class="container-fluid">
+         <div class="row">
+            <div class="col-sm-12">
+               <h1 class="service_taital">Our Services</h1>
+            </div>
+         </div>
+         <div class="service_section_2">
+            <div class="row">
+               <div class="col-lg-3 col-sm-6">
+                  <div class="service_box">
+                     <div class="building_icon"><img src="images/building-icon.png"></div>
+                     <h4 class="residential_text">Industrial Automation</h4>
+                     <p class="service_text">We deliver automation solutions to boost productivity and reduce manual
+                        work. Increase factory efficiency with our advanced systems.</p>
+                  </div>
+                  <div class="readmore_bt"><a href="#">Read More</a></div>
+               </div>
+               <div class="col-lg-3 col-sm-6">
+                  <div class="service_box">
+                     <div class="building_icon"><img src="images/building-icon.png"></div>
+                     <h4 class="residential_text">House Smart Automation</h4>
+                     <p class="service_text">Automate your home’s lighting, AC, power, and monitoring systems. Enjoy
+                        energy savings, comfort, and full remote control of your home.</p>
+                  </div>
+                  <div class="readmore_bt"><a href="#">Read More</a></div>
+               </div>
+               <div class="col-lg-3 col-sm-6">
+                  <div class="service_box">
+                     <div class="building_icon"><img src="images/building-icon.png"></div>
+                     <h4 class="residential_text">Fire Safety Equipment</h4>
+                     <p class="service_text">Equip your spaces with modern fire alarms and extinguishers. Ensure safety
+                        with reliable, certified fire protection systems.</p>
+                  </div>
+                  <div class="readmore_bt"><a href="#">Read More</a></div>
+               </div>
+               <div class="col-lg-3 col-sm-6">
+                  <div class="service_box">
+                     <div class="building_icon"><img src="images/building-icon.png"></div>
+                     <h4 class="residential_text">Parking Management System</h4>
+                     <p class="service_text">Smart parking solutions for smooth vehicle flow. Manage lots efficiently,
+                        increase convenience, and optimize parking spaces.</p>
+                  </div>
+                  <div class="readmore_bt"><a href="#">Read More</a></div>
+               </div>
+               <div class="col-lg-3 col-sm-6">
+                  <div class="service_box">
+                     <div class="building_icon"><img src="images/building-icon.png"></div>
+                     <h4 class="residential_text">Solar Installation & Consultancy</h4>
+                     <p class="service_text">Switch to solar with expert guidance and installation. Save energy costs
+                        and go green with our customized solar solutions.</p>
+                  </div>
+                  <div class="readmore_bt"><a href="#">Read More</a></div>
+               </div>
+               <div class="col-lg-3 col-sm-6">
+                  <div class="service_box">
+                     <div class="building_icon"><img src="images/bulb-icon.png"></div>
+                     <h4 class="residential_text">Energy Audit</h4>
+                     <p class="service_text">Identify energy waste and optimize your power use. Our audits help you save
+                        money and improve overall system performance.</p>
+                  </div>
+                  <div class="readmore_bt"><a href="#">Read More</a></div>
+               </div>
+               <div class="col-lg-3 col-sm-6">
+                  <div class="service_box">
+                     <div class="building_icon"><img src="images/building-icon.png"></div>
+                     <h4 class="residential_text">Biomedical Equipment</h4>
+                     <p class="service_text">High-quality biomedical equipment supply and maintenance. Supporting
+                        healthcare professionals with reliable medical devices.</p>
+                  </div>
+                  <div class="readmore_bt"><a href="#">Read More</a></div>
+               </div>
+               <div class="col-lg-3 col-sm-6">
+                  <div class="service_box">
+                     <div class="building_icon"><img src="images/building-icon.png"></div>
+                     <h4 class="residential_text">Industrial Machine Supply & Modification</h4>
+                     <p class="service_text">Get industrial machines built to your needs. We also upgrade and modify
+                        machines to maximize performance and lifespan.</p>
+                  </div>
+                  <div class="readmore_bt"><a href="#">Read More</a></div>
+               </div>
+               <div class="col-lg-3 col-sm-6">
+                  <div class="service_box">
+                     <div class="building_icon"><img src="images/flash-icon.png"></div>
+                     <h4 class="residential_text">MEP Consultancy</h4>
+                     <p class="service_text">Expert advice on Mechanical, Electrical, and Plumbing systems. We ensure
+                        efficient, safe, and sustainable building services.</p>
+                  </div>
+                  <div class="readmore_bt"><a href="#">Read More</a></div>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+   <!-- service section end -->
+   <!-- about section start -->
+   <div class="about_section layout_padding">
+      <div class="container">
+         <div class="row">
+            <div class="col-sm-12">
+               <h1 class="about_taital">About Us</h1>
+            </div>
+         </div>
+      </div>
+   </div>
+   <div class="about_section_2 layout_padding">
+      <div class="container">
+         <div class="row">
+            <div class="col-md-6">
+               <div class="about_taital_main">
+                  <h1 class="slightly_text">Let’s build the future together.</h1>
+                  <p class="lorem_text">Established in 2015, Frontier Engineering & Automation Consultancy (FEAC) provides innovative engineering and automation solutions tailored to modern industry needs. We specialize in smart system design, industrial automation, and project consultancy — helping businesses enhance efficiency, precision, and performance.</p>
+                  <p class="lorem_text">With a focus on innovation and reliability, FEAC is committed to turning challenges into smart solutions that drive progress.</p>
+                  <div class="readmore_btn"><a href="#">Read More</a></div>
+               </div>
+            </div>
+            <div class="col-md-6">
+               <div class="about_img"><img src="images/img-2.jpg"></div>
+            </div>
+         </div>
+      </div>
+   </div>
+   <!-- about section end -->
+   <!-- testimonial section start -->
+   <!-- <div class="testimonial_section layout_padding">
+      <div class="container">
+         <div class="row">
+            <div class="col-sm-12">
+               <h1 class="testimonial_taital">Customers Testimonial</h1>
+               <p class="testimonial_text">Even slightly believable. If you are going to use a passage </p>
+            </div>
+         </div>
+         <div id="my_slider" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+               <div class="carousel-item active">
+                  <div class="testimonial_section_2">
+                     <div class="testimonial_left">
+                        <div class="client_img"><img src="images/client-img.png"></div>
+                     </div>
+                     <div class="testimonial_right">
+                        <h4 class="markro_text">Markro</h4>
+                        <p class="many_text">many variations of passages of Lorem Ipsum available, but the majority have
+                           suffered alteration in some form, by injected humour, or randomised words which don't look
+                           even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be
+                           sure there isn't anything embarrassing hidden in the middle </p>
+                        <div class="quick_icon"><img src="images/quick-icon.png"></div>
+                     </div>
+                  </div>
+               </div>
+               <div class="carousel-item">
+                  <div class="testimonial_section_2">
+                     <div class="testimonial_left">
+                        <div class="client_img"><img src="images/client-img.png"></div>
+                     </div>
+                     <div class="testimonial_right">
+                        <h4 class="markro_text">Markro</h4>
+                        <p class="many_text">many variations of passages of Lorem Ipsum available, but the majority have
+                           suffered alteration in some form, by injected humour, or randomised words which don't look
+                           even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be
+                           sure there isn't anything embarrassing hidden in the middle </p>
+                        <div class="quick_icon"><img src="images/quick-icon.png"></div>
+                     </div>
+                  </div>
+               </div>
+               <div class="carousel-item">
+                  <div class="testimonial_section_2">
+                     <div class="testimonial_left">
+                        <div class="client_img"><img src="images/client-img.png"></div>
+                     </div>
+                     <div class="testimonial_right">
+                        <h4 class="markro_text">Markro</h4>
+                        <p class="many_text">many variations of passages of Lorem Ipsum available, but the majority have
+                           suffered alteration in some form, by injected humour, or randomised words which don't look
+                           even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be
+                           sure there isn't anything embarrassing hidden in the middle </p>
+                        <div class="quick_icon"><img src="images/quick-icon.png"></div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <a class="carousel-control-prev" href="#my_slider" role="button" data-slide="prev">
+               <i class="fa fa-angle-left"></i>
+            </a>
+            <a class="carousel-control-next" href="#my_slider" role="button" data-slide="next">
+               <i class="fa fa-angle-right"></i>
+            </a>
+         </div>
+      </div>
+   </div> -->
+   <!-- testimonial section end -->
+   <!-- shop section start -->
+   <div class="shop_section layout_padding">
+      <div class="container">
+         <div class="row">
+            <div class="col-md-12">
+               <h1 class="shop_taital">Do you need any help? <span style="color: #f9b116;"></span></h1>
+               <p class="shop_text">We're here to assist you reach out anytime for expert guidance and support.</p>
+               <div class="btn_main">
+                  <div class="quote_bt"><a href="#">Get A Quote</a></div>
+                  <div class="call_bt active"><a href="contact.html">Call Now</a></div>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+   <!-- shop section end -->
+   <!-- contact section start -->
+   <div class="contact_section layout_padding">
+      <div class="container-fluid">
+         <div class="contact_section_2">
+            <div class="row">
+               <div class="col-md-6">
+                  <h1 class="contact_taital">Get In Touch</h1>
+                  <form action="">
+                     <div class="mail_section_1">
+                        <input type="text" class="mail_text" placeholder="Name" name="Name">
+                        <input type="text" class="mail_text" placeholder="Phone Number" name="Phone Number">
+                        <input type="text" class="mail_text" placeholder="Email" name="Email">
+                        <textarea class="massage-bt" placeholder="Massage" rows="5" id="comment"
+                           name="Massage"></textarea>
+                        <div class="send_bt"><a href="#">SEND</a></div>
+                     </div>
+                  </form>
+               </div>
+               <div class="col-md-6 padding_right0">
+                  <div class="map_main">
+                     <div class="map-responsive">
+                        <iframe
+                           src="https://www.google.com/maps/embed/v1/place?key=AIzaSyA0s1a7phLN0iaD6-UE7m4qP-z21pH0eSc&amp;q=Eiffel+Tower+Paris+France"
+                           width="600" height="470" frameborder="0" style="border:0; width: 100%;"
+                           allowfullscreen=""></iframe>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+   <!-- contact section end -->
+   <!-- footer section start -->
+   <div class="footer_section">
+      <div class="container">
+         <div class="footer_sectio_2">
+            <div class="row">
+               <div class="col-lg-4 col-md-4">
+                  <h2 class="useful_text">Quick Links</h2>
+                  <div class="footer_menu">
+                     <ul>
+                        <li class="active"><a href="index.html">Home</a></li>
+                        <li><a href="about.html">About</a></li>
+                        <li><a href="service.html">Services</a></li>
+                        <!-- <li><a href="blog.html">Blog</a></li>
+                        <li><a href="testimonial.html">Testimonial</a></li> -->
+                        <li><a href="contact.html">Contact Us</a></li>
+                     </ul>
+                  </div>
+               </div>
+               <div class="col-lg-4 col-md-4">
+                  <h2 class="useful_text">Blog</h2>
+                  <p class="footer_text">There are many variations of passages of Lorem Ipsum available, but the
+                     majority have suffered</p>
+                  <!-- <div class="social_icon">
+                     <ul>
+                        <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                        <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                        <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+                     </ul>
+                  </div> -->
+               </div>
+               <div class="col-lg-4 col-md-4">
+                  <h2 class="useful_text">Newsletter</h2>
+                  <input type="text" class="Enter_text" placeholder="Enter Your Email" name="Enter Your Email">
+                  <div class="subscribe_bt"><a href="#">Subscribe</a></div>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+   <!-- footer section end -->
+   <!-- copyright section start -->
+   <div class="copyright_section">
+      <div class="container">
+         <p class="copyright_text">Frontier Engineering All Rights Reserved. <a href="#"></p>
+      </div>
+   </div>
+   <!-- copyright section end -->
+   <!-- Javascript files-->
+   <script src="js/jquery.min.js"></script>
+   <script src="js/popper.min.js"></script>
+   <script src="js/bootstrap.bundle.min.js"></script>
+   <script src="js/jquery-3.0.0.min.js"></script>
+   <script src="js/plugin.js"></script>
+   <!-- sidebar -->
+   <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
+   <script src="js/custom.js"></script>
+</body>
+
+</html>
